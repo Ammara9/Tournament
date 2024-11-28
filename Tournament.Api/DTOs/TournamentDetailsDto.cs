2cd7734d1ed2d192e460a68ed83f8fb578955b20
Ammara Shakeel <ammarashakeel81@yahoo.se>
2024-11-28 18:46:13 +0100

    DTOs

---
+++ Tournament.Api/DTOs/TournamentDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Tournament.Api.DTOs
+{
+    public record TournamentDetailsDto
+    {
+        public int Id { get; init; }
+        public string? Title { get; init; }
+
+        public DateTime StartDate { get; init; }
+    }
+}
